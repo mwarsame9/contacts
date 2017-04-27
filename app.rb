1cf03049f8ab92c3d87f3d7470eb2a53174eac48
Mohamed Warsame and Susha Dore <mohamed@>
2017-04-27 11:13:14 -0700

    take contact info and return it in list form on index page

--- app.rb
+++ app.rb
@@ -4,5 +4,17 @@ also_reload 'lib/**/*.rb'
 require './lib/contacts'
 
 get('/') do
+  @contacts = Contact.all
   erb(:index)
 end
+
+get('/new_contact') do
+  erb :new_contact_form
+end
+
+ post('/new_contact') do
+   @contact = Contact.new(:first_name => params.fetch('first_name'), :last_name => params.fetch('last_name'), :job_title => params.fetch('job_title'), :company => params.fetch('company'))
+   @contact.save
+   redirect '/new_contact'
+   erb :new_contact_form
+ end
